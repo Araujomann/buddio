de634d3df963ed01dc9f362a13844b47a94df7a5
Araujomann <lucastheeyepatch@gmail.com>
2025-01-04 14:28:58 -0300

    fix: little adjustments in the welcome page quotes

--- buddio/src/pages/welcome/Welcome.tsx
+++ buddio/src/pages/welcome/Welcome.tsx
@@ -3,19 +3,17 @@ import { useSpring, animated } from '@react-spring/web';
 import { Header, Button, Footer } from '../../components';
 import cam from '../../assets/cam.jpg';
 import image2 from '../../assets/buddioImage3.jpeg';
-import image3 from '../../assets/buddioImage1.jpeg';
+import image3 from '../../assets/buddioImage6.jpeg';
 import { useNavigate } from 'react-router-dom';
 
 export const Welcome: React.FC = () => {
   const navigate = useNavigate();
 
- 
   const titleAnimation = useSpring({
     from: { opacity: 0, transform: 'translateY(20px)' },
     to: { opacity: 1, transform: 'translateY(0px)' },
     config: { duration: 800 },
   });
-
 
   const buttonsAnimation = useSpring({
     from: { opacity: 0, transform: 'scale(0.9)' },
@@ -23,7 +21,6 @@ export const Welcome: React.FC = () => {
     delay: 500,
     config: { tension: 120, friction: 14 },
   });
-
 
   const imageAnimation = useSpring({
     from: { transform: 'scale(0.8)' },
@@ -43,14 +40,12 @@ export const Welcome: React.FC = () => {
     config: { duration: 2200 },
   });
 
- 
   const textAnimation = useSpring({
     from: { opacity: 0, transform: 'translateX(-50px)' },
     to: { opacity: 1, transform: 'translateX(0)' },
     delay: 800,
   });
 
-  
   const quoteAnimation = useSpring({
     from: { opacity: 0, transform: 'translateY(20px)' },
     to: { opacity: 1, transform: 'translateY(0px)' },
@@ -72,7 +67,6 @@ export const Welcome: React.FC = () => {
     config: { duration: 800 },
   });
 
-  
   const authorAnimation = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
@@ -90,7 +84,7 @@ export const Welcome: React.FC = () => {
   const author3Animation = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
-    delay: 4100,
+    delay: 4300,
     config: { duration: 600 },
   });
 
@@ -103,10 +97,9 @@ export const Welcome: React.FC = () => {
   };
 
   return (
-    <div className="bg-buddyGray flex flex-col w-screen overflow-x-hidden h-full  items-center justify-start">
+    <div className="bg-buddyGray flex flex-col w-full  overflow-x-hidden h-full  items-center justify-start">
       <Header />
 
-      
       <animated.div style={titleAnimation} className="w-11/12 mt-24">
         <h1 className="font-montserrat text-3xl text-white font-bold">
           YOUR PLACE FOR AUTHENTIC PORTRAITS
@@ -117,7 +110,6 @@ export const Welcome: React.FC = () => {
         </p>
       </animated.div>
 
-   
       <animated.div
         style={buttonsAnimation}
         className="flex items-center gap-4 mt-12"
@@ -137,7 +129,7 @@ export const Welcome: React.FC = () => {
         />
       </animated.div>
 
-      <div className="relative flex w-full md:w-auto mt-14 justify-between mx-10">
+      <div className="relative flex w-full md:w-auto mt-14 justify-between mx-10 lg:px-20 xl:px-32">
         <div className="flex items-center justify-center w-full">
           <animated.div style={imageAnimation} className="relative flex">
             <animated.h2
@@ -150,29 +142,29 @@ export const Welcome: React.FC = () => {
           </animated.div>
         </div>
 
-        <div className="hidden md:flex flex-col items-start  ">
+        <div className="hidden md:flex flex-col items-start justify-center ">
           <animated.p
             style={quoteAnimation}
-            className="font-montserrat flex  text-2xl lg:text-3xl pl-10 py-6 font-semibold  lg:text-justify leading-tight text-white"
+            className="font-montserrat flex items-center text-xl lg:text-2xl pl-10 py-6 font-semibold  lg:text-justify leading-tight text-white"
           >
-            "A beleza está nos detalhes do horizonte, na luz que dança sobre as
-            montanhas, e nas cores que o céu pinta ao entardecer. Aqui no
-            Buddio, cada paisagem é um convite para contemplar o mundo com olhos
-            de gratidão."
+            "Este mundo natural é uma catedral, uma vastidão de beleza e
+            harmonia. Cada folha de grama, cada raio de luz e sombra são
+            obras-primas, esperando serem vistas por aqueles que se permitem ser
+            tocados pelo divino na simplicidade."
           </animated.p>
           <animated.span
             style={authorAnimation}
-            className="absolute bottom-1 right-1 font-montserrat text-sm lg:right-6"
+            className="absolute bottom-1 right-1 lg:right-20 xl:right-32 font-montserrat text-sm"
           >
-            Autor desconhecido
+            John Muir
           </animated.span>
         </div>
       </div>
 
-      <div className="relative flex w-full flex-row-reverse md:w-auto mt-24 justify-between mx-10">
+      <div className="hidden md:flex relative  w-full flex-row-reverse md:w-auto mt-24 justify-between mx-10 lg:px-20 xl:px-32">
         <div className="flex items-center justify-center w-full">
           <animated.div style={image2Animation} className="relative flex">
-            <img src={image2} className="max-w-96" />
+            <img src={image2} className="max-w-96 md:py-4 lg:py-9 bg-white" />
           </animated.div>
         </div>
 
@@ -181,41 +173,41 @@ export const Welcome: React.FC = () => {
             style={quote2Animation}
             className="relative font-montserrat  flex text-xl lg:text-2xl pr-10 py-6 font-semibold  lg:text-justify leading-tight text-white"
           >
-            "Nos detalhes mais simples se escondem histórias grandiosas. Um
-            reflexo na poça d'água, a sombra de uma árvore ou o brilho das
-            folhas ao sol – momentos assim nos lembram que o extraordinário vive
-            no cotidiano."
+            "Acordei esta manhã com um devoto reconhecimento pela beleza ao meu
+            redor. O céu estava puro e sereno, e toda a terra parecia em
+            harmonia. Não há como passar pelo dia sem um espírito de
+            contemplação por tudo que é belo e verdadeiro."
             <animated.span
               style={author2Animation}
-              className="absolute right-4 md:bottom-4 lg:bottom-0 lg:right-10 font-montserrat text-sm font-normal text-white"
+              className="absolute right-4 md:bottom-4 lg:bottom-4 lg:right-10  font-montserrat text-sm font-normal text-pretty text-white"
             >
-              Autor desconhecido
+              Henry David Thoreau
             </animated.span>
           </animated.p>
         </div>
       </div>
 
-      <div className="relative flex w-full  md:w-auto my-24 justify-between mx-10">
+      <div className="hidden md:flex relative  w-full  md:w-auto my-24 justify-between mx-10 lg:px-20 xl:px-32">
         <div className="flex items-center justify-center w-full">
           <animated.div style={image3Animation} className="relative flex">
             <img src={image3} className="max-w-96" />
           </animated.div>
         </div>
-        <div className="  hidden md:flex flex-col items-start justify-center">
+        <div className="hidden md:flex flex-col items-start justify-center">
           <animated.p
             style={quote3Animation}
             className="relative font-montserrat  flex text-lg lg:text-2xl pl-10 py-3 font-semibold lg:text-justify leading-tight text-white"
           >
-            "O mundo ainda guarda tanta beleza para ser descoberta. Cada foto é
-            um registro do presente, um lembrete de que a criação nos presenteia
-            diariamente com maravilhas que nos conectam ao divino e ao que
-            realmente importa."
+            "Às vezes, é impossível expressar em palavras como um simples campo
+            de trigo sob um céu azul pode conter toda a imensidão da vida e da
+            alma. Mas, quando você olha, sente que é bonito, sente que é
+            eterno."
           </animated.p>
           <animated.span
             style={author3Animation}
-            className="absolute right-4 md:bottom-0  lg:right-0 font-montserrat text-sm font-normal text-white"
+            className="absolute right-4 md:bottom-0  lg:right-20 xl:right-32 font-montserrat text-sm font-normal text-white"
           >
-            Autor desconhecido
+            Vincent van Gogh
           </animated.span>
         </div>
       </div>
