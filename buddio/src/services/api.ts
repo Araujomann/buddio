1cece8db9c62aaff213660553d70b2e54579dd33
Araujomann <lucastheeyepatch@gmail.com>
2025-01-11 18:42:48 -0300

    fix

--- buddio/src/services/api.ts
+++ buddio/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://buddio-backend-production.up.railway.app:8080',
+  baseURL: 'https://buddio-backend-production.up.railway.app',
   withCredentials: true,
 });
