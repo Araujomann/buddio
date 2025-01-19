b78b53535805e4a0158cd5f2a657e29c1fa5153f
Araujomann <lucastheeyepatch@gmail.com>
2025-01-18 22:18:11 -0300

    fix?: solving browser problem about process is not define

--- buddio/src/services/api.ts
+++ buddio/src/services/api.ts
@@ -1,8 +1,6 @@
-import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.DEVELOPMENT_API_URL,
-  withCredentials: true,
+    baseURL: import.meta.env.PRODUCTION_API_URL,
+    withCredentials: true,
 });
