2b87bd0253af315327365ca84ce0375873428962
Araujomann <lucastheeyepatch@gmail.com>
2025-02-04 14:55:30 -0300

    fix: setting new Firebase credentials for google auth

--- buddio/src/services/googleAuthConfig.ts
+++ buddio/src/services/googleAuthConfig.ts
@@ -1,13 +1,19 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBb0cIIiKdatwvybwcBq6imHeKva7Iq5FM',
-  authDomain: 'buddio-8194f.firebaseapp.com',
-  projectId: 'buddio-8194f',
-  storageBucket: 'buddio-8194f.firebasestorage.app',
-  messagingSenderId: '78891245479',
-  appId: '1:78891245479:web:538ddc7d6b43a9563ce082',
-  measurementId: 'G-L9MFGGD1ZY',
+    apiKey: 'AIzaSyAGqFxCv2UVw7HeIQgGIl1imqrr3pPru10',
+    authDomain: 'buddio-8194f.firebaseapp.com',
+    projectId: 'buddio-8194f',
+    storageBucket: 'buddio-8194f.firebasestorage.app',
+    messagingSenderId: '78891245479',
+    appId: '1:78891245479:web:f2b5127a3e9a6a683ce082',
+    measurementId: 'G-4769X8362J',
 };
 
-export const App = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
